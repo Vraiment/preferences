cf9c4dc861ad93a4cb528e85ec0e772862161b16
Vraiment <jemc44@gmail.com>
2018-08-08 18:09:31 -0700

    Updated env_utils to be able to use it with variables other than PATH (for example CLASSPATH)

--- Scripts/common_bashrc.sh
+++ Scripts/common_bashrc.sh
@@ -1,5 +1,5 @@
 # Add additional entries to PATH
-add_to_path_front "$HOME/bin"
+pathprepend "$HOME/bin"
 
 # Common aliases
 alias vim='vim -p'
--- Scripts/env_utils.sh
+++ Scripts/env_utils.sh
@@ -1,27 +1,71 @@
-# List the entries in the $PATH variable
-function lspath {
-    tr ':' "\n" <<< $PATH
-}
-
-# Searchs for the first argument in the $PATH variable
-function in_path {
-    lspath | grep "^$1$"
-}
-
-# Adds the first argument in front of the path variable
-function add_to_path_front {
-    if [ $(in_path $1) ]; then
-        return 1
+# Adds lists the entries in the variable with the name of the first argument
+# ex: lsevar PATH
+function lsevar {
+    if [[ $# -ne 1 || -z ${!1+x} ]]; then
+        return $(false)
     fi
     
-    export PATH="$1:$PATH"
+    tr ':' "\n" <<< ${!1}
 }
 
-# Adds the first argument in the back of the path variable
-function add_to_path_back {
-    if [ $(in_path $1) ]; then
-        return 1
+# Verifies if the second argument is in the variable with the name of the first argument
+# ex: grepevar PATH /bin
+function grepevar {
+    if [ $# -ne 2 ]; then
+        return $(false)
     fi
     
-    export PATH="$PATH:$1"
+    lsevar $1 | grep "^$2$"
+}
+
+# Adds a new entry in the environment variable at the beginning of the variable
+# ex: evarprepend PATH $HOME/bin
+function evarprepend {
+    if [ $# -ne 2 ]; then
+        return $(false)
+    fi
+    
+    if [ -z ${!1} ]; then
+        eval "$1=\$2"
+    else
+        eval "$1=\$2:\${!1}"
+    fi
+}
+
+# Adds a new entry in the environment variable at the beginning of the variable
+# ex: evarappend PATH $HOME/bin
+function evarappend {
+    if [ $# -ne 2 ]; then
+        return $(false)
+    fi
+    
+    if [ -z ${!1} ]; then
+        eval "$1=\$2"
+    else
+        eval "$1=\${!1}:\$2"
+    fi
+}
+
+# PATH related commands
+alias lspath='lsevar PATH'
+alias greppath='grepevar PATH'
+
+# Adds the first argument to the front of PATH if is not present
+# ex: pathprepend $HOME/bin
+function pathprepend {
+    if [[ $# -ne 1 || $(greppath $1) ]]; then
+        return $(false)
+    fi
+    
+    evarprepend PATH $1
+}
+
+# Adds the first argument to the back of PATH if is not present
+# ex: pathappend /usr/local/bin
+function pathappend {
+    if [[ $# -ne 1 || $(greppath $1) ]]; then
+        return $(false)
+    fi
+    
+    evarappend PATH $1
 }
