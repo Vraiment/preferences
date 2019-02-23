20d08d940963c9fd415f5c2679c79e96dde42b3d
Vraiment <jemc44@gmail.com>
2019-02-22 18:37:30 -0800

    Add code to add support for rbenv

--- Scripts/macOS_bashrc.sh
+++ Scripts/macOS_bashrc.sh
@@ -1,2 +1,22 @@
 # Enable colors in terminal
 export CLICOLOR=1
+
+# Enable rbenv
+if command -v rbenv > /dev/null; then
+    pathprepend "$HOME/.rbenv/shims"
+    export RBENV_SHELL=bash
+    command rbenv rehash
+
+    rbenv() {
+        local command
+        command="$1"
+        if [ "$#" -gt 0 ]; then
+            shift
+        fi
+
+        case "$command" in
+            rehash|shell) eval "$(rbenv "sh-$command" "$@")";;
+            *) command rbenv "$command" "$@";;
+        esac
+    }
+fi
