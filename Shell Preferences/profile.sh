d30f31129a6ce2cc425bc74227c25ddd9a1e149e
Vraiment <jemc44@gmail.com>
2019-07-18 13:13:05 -0700

    Add configuration for "rbenv"

--- Shell Preferences/profile.sh
+++ Shell Preferences/profile.sh
@@ -23,6 +23,29 @@ if [ -n "$BASH_VERSION" ]; then
 	source "$HOME/.bashrc"
 fi
 
+# Configure "rbenv", for version 1.1.2
+if [ -f "/usr/local/bin/rbenv" ]; then
+    export PATH="$HOME/.rbenv/shims:${PATH}"
+    export RBENV_SHELL=bash
+
+    command rbenv rehash 2>/dev/null
+
+    rbenv() {
+        local command
+        command="${1:-}"
+        if [ "$#" -gt 0 ]; then
+            shift
+        fi
+
+        case "$command" in
+        rehash|shell)
+            eval "$(rbenv "sh-$command" "$@")";;
+        *)
+            command rbenv "$command" "$@";;
+        esac
+    }
+fi
+
 # Adds user's personal bin directory
 if [ -d "$HOME/bin" ] ; then
     pathprepend "$HOME/bin"
