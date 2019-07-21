20b0881b35f646784373897f863789c1d5135952
Vraiment <jemc44@gmail.com>
2019-07-21 12:35:40 -0700

    Add colors for 'ls' command

--- Shell Preferences/env-variables.sh
+++ Shell Preferences/env-variables.sh
@@ -16,3 +16,6 @@ case "$(uname -s)" in
         export CLICOLOR=1
         ;;
 esac
+
+# Set the colors for LS
+export LS_COLORS="di=34:ln=35:so=31:pi=36:ex=32:bd=30;46:cd=30;46:su=32:sg=32:tw=34;46:ow=34"
