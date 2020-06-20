079cfefa997bef8c629830a81b66dde0bbf39cee
Vraiment <jemc44@gmail.com>
2020-06-20 12:00:21 -0700

    Update print_colors.sh so it passes ShellCheck

--- Scripts/print_colors.sh
+++ Scripts/print_colors.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT_GRAY='\033[0;37m'
-DARK_GRAY='\033[1;30m'
-LIGHT_RED='\033[1;31m'
-LIGHT_GREEN='\033[1;32m'
-LIGHT_YELLOW='\033[1;33m'
-LIGHT_BLUE='\033[1;34m'
-LIGHT_MAGENTA='\033[1;35m'
-LIGHT_CYAN='\033[1;36m'
-WHITE='\033[1;37m'
-NO_COLOR='\033[0m'
+export BLACK='\033[0;30m'
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export MAGENTA='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT_GRAY='\033[0;37m'
+export DARK_GRAY='\033[1;30m'
+export LIGHT_RED='\033[1;31m'
+export LIGHT_GREEN='\033[1;32m'
+export LIGHT_YELLOW='\033[1;33m'
+export LIGHT_BLUE='\033[1;34m'
+export LIGHT_MAGENTA='\033[1;35m'
+export LIGHT_CYAN='\033[1;36m'
+export WHITE='\033[1;37m'
+export NO_COLOR='\033[0m'
 
-printf "${BLACK}BLACK${NO_COLOR}\n"
-printf "${RED}RED${NO_COLOR}\n"
-printf "${GREEN}GREEN${NO_COLOR}\n"
-printf "${YELLOW}YELLOW${NO_COLOR}\n"
-printf "${BLUE}BLUE${NO_COLOR}\n"
-printf "${MAGENTA}MAGENTA${NO_COLOR}\n"
-printf "${CYAN}CYAN${NO_COLOR}\n"
-printf "${LIGHT_GRAY}LIGHT_GRAY${NO_COLOR}\n"
-printf "${DARK_GRAY}DARK_GRAY${NO_COLOR}\n"
-printf "${LIGHT_RED}LIGHT_RED${NO_COLOR}\n"
-printf "${LIGHT_GREEN}LIGHT_GREEN${NO_COLOR}\n"
-printf "${LIGHT_YELLOW}LIGHT_YELLOW${NO_COLOR}\n"
-printf "${LIGHT_BLUE}LIGHT_BLUE${NO_COLOR}\n"
-printf "${LIGHT_MAGENTA}LIGHT_MAGENTA${NO_COLOR}\n"
-printf "${LIGHT_CYAN}LIGHT_CYAN${NO_COLOR}\n"
-printf "${WHITE}WHITE${NO_COLOR}\n"
+function printColor() {
+    local COLOR=$1
+
+    printf "${!COLOR}%s${NO_COLOR}\n" "$COLOR"
+}
+
+printColor BLACK
+printColor RED
+printColor GREEN
+printColor YELLOW
+printColor BLUE
+printColor MAGENTA
+printColor CYAN
+printColor LIGHT_GRAY
+printColor DARK_GRAY
+printColor LIGHT_RED
+printColor LIGHT_GREEN
+printColor LIGHT_YELLOW
+printColor LIGHT_BLUE
+printColor LIGHT_MAGENTA
+printColor LIGHT_CYAN
+printColor WHITE
